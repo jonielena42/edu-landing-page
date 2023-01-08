9799f0b5bbcccd97706ecdc11c4cc25c0db0ef56
jonielena42 <122189696+jonielena42@users.noreply.github.com>
2023-01-08 00:55:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const toggleButton = document.getElementById("toggle-button");
 const nav = document.getElementById("nav");
 
 toggleButton.addEventListener("click", () => {
+  console.log(toggleButton);
+  console.log(nav);
   nav.classList.toggle("open");
 });
