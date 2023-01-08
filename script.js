00a5dab69399d7318418b832f84390b1cd08df97
jonielena42 <122189696+jonielena42@users.noreply.github.com>
2023-01-08 00:21:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function toggleMenu() {
-  var menu = document.querySelector('.popout-menu');
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'block';
-  }
-}
+// Create the menu element
+var menu = document.createElement('div');
+menu.classList.add('popout-menu');
+menu.innerHTML = `
+  <div class="menu-items">
+    <a class="menu-item" href="#">Menu item 1</a>
+    <a class="menu-item" href="#">Menu item 2</a>
+    <a class="menu-item" href="#">Menu item 3</a>
+  </div>
+`;
+
+// Add the menu to the page
+document.body.appendChild(menu);
+
+// Create the menu toggle button
+var toggleButton = document.createElement('button');
+toggleButton.classList.add('menu-toggle-button');
+toggleButton.innerHTML = 'Projects';
+document.body.appendChild(toggleButton);
+
+// Add a click event listener to the toggle button
+toggleButton.addEventListener('click', function() {
+  // Toggle the 'menu-open' class on the menu
+  menu.classList.toggle('menu-open');
+});
