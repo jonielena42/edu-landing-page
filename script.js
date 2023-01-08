987d3e0bcee3c3ea63bb137d5e7e0c94d271b3b5
jonielena42 <122189696+jonielena42@users.noreply.github.com>
2023-01-08 00:29:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// Create the menu element
-var menu = document.createElement('div');
-menu.id = 'menu';
-menu.innerHTML = '<a href="#">Menu item 1</a><a href="#">Menu item 2</a><a href="#">Menu item 3</a>';
+const menu = document.getElementById("menu");
+const toggleButton = document.getElementById("toggle-button");
 
-// Add the menu to the page
-document.body.appendChild(menu);
-
-// Create the menu toggle button
-var toggleButton = document.createElement('button');
-toggleButton.id = 'toggle-button';
-toggleButton.innerHTML = 'Toggle menu';
-document.body.appendChild(toggleButton);
-
-// Add a click event listener to the toggle button
-toggleButton.addEventListener('click', function() {
-  // Toggle the 'menu-open' class on the menu
-  menu.classList.toggle('menu-open');
+toggleButton.addEventListener("click", () => {
+  menu.classList.toggle("menu-open");
 });
