14527c8182c4dffde1f924b221ba3fe0397dbe15
jonielena42 <122189696+jonielena42@users.noreply.github.com>
2023-01-08 00:23:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 // Create the menu element
 var menu = document.createElement('div');
-menu.classList.add('popout-menu');
-menu.innerHTML = `
-  <div class="menu-items">
-    <a class="menu-item" href="#">Menu item 1</a>
-    <a class="menu-item" href="#">Menu item 2</a>
-    <a class="menu-item" href="#">Menu item 3</a>
-  </div>
-`;
+menu.id = 'menu';
+menu.innerHTML = '<a href="#">Menu item 1</a><a href="#">Menu item 2</a><a href="#">Menu item 3</a>';
 
 // Add the menu to the page
 document.body.appendChild(menu);
 
 // Create the menu toggle button
 var toggleButton = document.createElement('button');
-toggleButton.classList.add('menu-toggle-button');
-toggleButton.innerHTML = 'Projects';
+toggleButton.id = 'toggle-button';
+toggleButton.innerHTML = 'Toggle menu';
 document.body.appendChild(toggleButton);
 
 // Add a click event listener to the toggle button
